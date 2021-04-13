ccd3e25ca696bf59dda7e08be9c555fe7b27be97
Leslie Gonzalez <lesligonzale2003@gmail.com>
2021-04-13 11:14:41 -0400

    Initial Commit

---
+++ Leslie_Seguesp2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Leslie_Seguesp2
+//
+//  Created by Leslie Gonzalez on 4/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
